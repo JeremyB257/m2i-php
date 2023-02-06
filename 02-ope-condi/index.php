b6fe89ecf37113ce2bf8730473b9b27716bcd44d
Jerem <bertelootjerem@gmail.com>
2023-02-06 13:56:08 +0100

    init 02

---
+++ 02-ope-condi/index.php
@@ -0,0 +1,25 @@
+<?php
+$number1 = 3;
+$number2 = 4;
+$number3 = 5;
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Le PHP</title>
+</head>
+
+<body>
+    <h2>Operation en PHP</h2>
+    <p>3 + 4 = <?= $number1 + $number2 ?></p>
+    <p>4 * 5 = <?= $number2 * $number3 ?></p>
+    <p>5 / 3 = <?= round($number3 / $number1, 2) ?></p>
+    <p>2 <sup>3</sup> = <?= 2 ** $number1 ?></p>
+</body>
+
+</html>
