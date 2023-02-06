f0068d703d221cd0ed783b041539d07969f27af8
Jerem <bertelootjerem@gmail.com>
2023-02-06 14:39:09 +0100

    maj

--- 02-ope-condi/index.php
+++ 02-ope-condi/index.php
@@ -2,6 +2,8 @@
 $number1 = 3;
 $number2 = 4;
 $number3 = 5;
+
+
 ?>
 
 <!DOCTYPE html>
@@ -20,6 +22,23 @@ $number3 = 5;
     <p>4 * 5 = <?= $number2 * $number3 ?></p>
     <p>5 / 3 = <?= round($number3 / $number1, 2) ?></p>
     <p>2 <sup>3</sup> = <?= 2 ** $number1 ?></p>
+
+    <?php
+    $sentence = 'Hello';
+    $sentence .= ' Fiorella'
+    ?>
+    <p><?= $sentence ?></p>
+
+    <h2>Comparaison</h2>
+    <p>est ce que number1 est strictement egale à 3 ? <?= var_dump($number1 === 3); ?></p>
+
+    <h2>Condition</h2>
+
+    <?php
+    $date = 6;
+
+    echo ($date === 7) ? 'On a TRE' : 'On a PHP';
+    ?>
 </body>
 
 </html>
