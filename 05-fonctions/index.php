59f996e6717d99b876bead51a1ce809434499a9f
Jerem <bertelootjerem@gmail.com>
2023-02-09 15:10:17 +0100

    maj exo

--- 05-fonctions/index.php
+++ 05-fonctions/index.php
@@ -25,6 +25,34 @@
     <h1><?= hello('Fiorella', 3) ?></h1>
     <h2><?= strtoUpper(hello('Toto')) ?></h2>
 
+
+    <?php
+    function square(int $n1): int {
+        return $n1 ** 2;
+    }
+
+    function squareArea(int $L, int $l): int {
+        return $L * $l;
+    }
+
+    function circleArea(int $r): float {
+        return round(($r ** 2) * pi(), 2);
+    }
+
+    function price(float $price, float $tax, string $type = 'htToTtc'): float {
+        if ($type == 'htToTtc') {
+            return round($price * (1 +  $tax / 100), 2);
+        } else {
+            return round($price / (1 + $tax / 100), 2);
+        }
+    }
+
+    ?>
+    <h3><?= square(5) ?></h3>
+    <h3><?= squareArea(5, 2) ?></h3>
+    <h3><?= circleArea(5) ?></h3>
+    <h3><?= price(100, 20, 'htToTtc') ?></h3>
+    <h3><?= price(100, 20, 'ttcToHt') ?></h3>
 </body>
 
 </html>
