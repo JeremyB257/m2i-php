603b243fa662a71ca8f2c5320b3003a0f22ab7dc
Jerem <bertelootjerem@gmail.com>
2023-02-10 09:37:07 +0100

    maj exo

--- 06-superGlobal/index.php
+++ 06-superGlobal/index.php
@@ -5,7 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
@@ -36,6 +39,27 @@
         <input type="text" name="age" placeholder="Age" value='<?= $age ?>'>
         <input type="submit" value="Envoyer">
     </form>
+
+    <?php
+    $repeat = $_GET['repeat'] ?? 10;
+    $text = [
+        'Je dois répéter cette phrase ' .  $repeat  . ' fois',
+        'Je suis une nouvelle phrase à recopier ' . $repeat . ' fois'
+    ];
+    $rand = rand(0, 1)
+    ?>
+
+    <h1>Bart</h1>
+    <form action="" method="get">
+        <input type="number" name="repeat" placeholder="Nombre">
+        <button>Envoyer</button>
+    </form>
+    <div class="board">
+        <img src="./bart.png" alt="barts">
+        <?php for ($i = 0; $i < $repeat; $i++) { ?>
+            <p><?= $text[$rand] ?></p>
+        <?php } ?>
+    </div>
 </body>
 
 </html>
