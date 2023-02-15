dbfbf460f564ada5685643007d372044e6ce5748
Jerem <bertelootjerem@gmail.com>
2023-02-15 15:20:38 +0100

    init

--- 08-chaines/index.php
+++ 08-chaines/index.php
@@ -1,5 +1,9 @@
 <?php
 
 $email = 'fiorella@boxydev.com';
-$domain = strstr($email, '@'); // @boxydev.com
-$domain = substr($domain, 1); // boxydev.com
+$domain = strstr($email, '@'); // @boxydev.com (enleve le contenu avant @)
+$domain = substr($domain, 1); // boxydev.com (enleve 1 lettre)
+$tld = strstr($domain, '.'); // .com
+
+echo "Le domaine est '$domain' <br/>";
+echo "Le tld de 1er niveau est '$tld' <br/>";
