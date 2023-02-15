59d62774b2a61cfa5f9f8e00b454afe1e20fb62b
Jerem <bertelootjerem@gmail.com>
2023-02-15 15:31:46 +0100

    exo

--- 08-chaines/index.php
+++ 08-chaines/index.php
@@ -1,9 +1,16 @@
 <?php
 
-$email = 'fiorella@boxydev.com';
-$domain = strstr($email, '@'); // @boxydev.com (enleve le contenu avant @)
-$domain = substr($domain, 1); // boxydev.com (enleve 1 lettre)
-$tld = strstr($domain, '.'); // .com
+//extraction 
+
+$email = 'fiorella@cloud.boxydev.com';
+$domain = strstr($email, '@'); // @cloud.boxydev.com (enleve le contenu avant @)
+$domain = substr($domain, 1); // cloud.boxydev.com (enleve 1 lettre)
+$tld = strrchr($domain, '.'); // .com (prend la derniere occurence du .)
 
 echo "Le domaine est '$domain' <br/>";
 echo "Le tld de 1er niveau est '$tld' <br/>";
+
+//Verifier qu'une chaine contient quelque chose
+$sentence = 'Faire ou ne pas faire.';
+
+echo 'Le mot faire est à la position ' . strpos($sentence, 'faire');
