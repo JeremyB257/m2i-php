8d829e11f162f5e3be76dc54f7976e877e8cde3e
Jerem <bertelootjerem@gmail.com>
2023-02-15 15:39:52 +0100

    exo

--- 08-chaines/index.php
+++ 08-chaines/index.php
@@ -13,4 +13,15 @@ echo "Le tld de 1er niveau est '$tld' <br/>";
 //Verifier qu'une chaine contient quelque chose
 $sentence = 'Faire ou ne pas faire.';
 
-echo 'Le mot faire est à la position ' . strpos($sentence, 'faire');
+echo 'Le mot faire est à la position ' . mb_strpos($sentence, 'faire');
+
+var_dump(strpos($sentence, 'rien'));
+
+// important de verifier le stric egale si le mot est a la position 0
+if (stripos($sentence, 'fai') !== false) {
+    echo 'OK </br>';
+}
+
+//remplacer une chaine dans une chaine
+
+echo str_replace('boxydev', 'gmail', $email);
