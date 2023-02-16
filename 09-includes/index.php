b899fb713a19c5429b3cc16d923ea1faa8d4543c
Jerem <bertelootjerem@gmail.com>
2023-02-16 13:14:44 +0100

    exo

--- 09-includes/index.php
+++ 09-includes/index.php
@@ -3,7 +3,7 @@ require __DIR__ . '\partials\header.php';
 
 ?>
 <main class="container-lg">
-    <h1>Acuueil</h1>
+    <h1>Acueil</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi neque, animi illum cumque accusamus dolorem rerum quas. Reprehenderit quod voluptatum voluptatem assumenda explicabo totam velit, eligendi nihil, hic repudiandae saepe.
         Ex quaerat necessitatibus earum eum doloremque recusandae aut ullam rem aliquam, aspernatur ducimus iure libero pariatur accusamus dolorum sed fugiat. Deleniti enim odio laboriosam debitis dolorem rerum labore eaque omnis.</p>
     <p>
