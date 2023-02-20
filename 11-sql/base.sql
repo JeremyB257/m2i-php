b8d1cd26e03f4c1ad9e86e12761d44b5458bc0e6
Jerem <bertelootjerem@gmail.com>
2023-02-20 15:33:01 +0100

    sql

--- 11-sql/base.sql
+++ 11-sql/base.sql
@@ -1,2 +1,8 @@
 -- Inserer une donnée en SQL
 INSERT INTO user (username, email, password) VALUES ('Fiorella', 'fiorella@gmail.com', 'password');
+
+-- Inserer plusieurs données d'un seul coup
+INSERT INTO user (username, email, password) VALUES
+('Toto', 'toto@gmail.com', 'password'), 
+('titi', 'titi@gmail.com', 'password'),
+('tata', 'tata@gmail.com', 'password');
