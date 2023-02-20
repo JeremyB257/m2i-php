7f97bc1f502e975917b27a7e82152c0c40b8a8a3
Jerem <bertelootjerem@gmail.com>
2023-02-20 16:13:20 +0100

    sql

--- 11-sql/base.sql
+++ 11-sql/base.sql
@@ -6,3 +6,12 @@ INSERT INTO user (username, email, password) VALUES
 ('Toto', 'toto@gmail.com', 'password'), 
 ('titi', 'titi@gmail.com', 'password'),
 ('tata', 'tata@gmail.com', 'password');
+
+-- on Peut mettre a jour une ou plusieur ligne (tuples)
+UPDATE user SET 'password' = 'daddy' WHERE id = 1;
+
+-- selectionner tous les utilisateur avec tous les champs
+SELECT * FROM `user`;
+
+-- selectionner tous les utilisateur avec le pseudo et l'email
+SELECT * FROM `user` WHERE username, email;
