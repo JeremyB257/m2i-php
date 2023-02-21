a4e8e25c78466cf92269fe3957abcc8324066143
Jerem <bertelootjerem@gmail.com>
2023-02-21 09:30:23 +0100

    sql

--- 11-sql/base.sql
+++ 11-sql/base.sql
@@ -18,3 +18,12 @@ SELECT * FROM `user` WHERE username, email;
 
 -- Supprimer une ligne dans la BDD
 DELETE FROM user WHERE id = 2;
+
+-- Le SELECT permet de récupérer les données dans une table
+SELECT * FROM movies;
+
+-- WHERE permet de filtrer et condiotionner
+-- On veux les fils qui durent plus de 3h
+SELECT * FROM movies WHERE duration >= 180;
+-- On veut les film d'action
+SELECT * FROM movies WHERE id_category = 2 AND duration >60
