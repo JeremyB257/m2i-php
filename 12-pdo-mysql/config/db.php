447cfdc29df84874b2f8bd1d712de8a785529dba
Jerem <bertelootjerem@gmail.com>
2023-02-22 11:50:44 +0100

    db connection

--- 12-pdo-mysql/config/db.php
+++ 12-pdo-mysql/config/db.php
@@ -1,2 +1,7 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=webflix', 'root', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'webflix');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+
+$db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
