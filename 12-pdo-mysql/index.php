d09beb0e3257df6005e71b1442f74057ba6482ff
Jerem <bertelootjerem@gmail.com>
2023-02-22 13:55:12 +0100

    fetch data

--- 12-pdo-mysql/index.php
+++ 12-pdo-mysql/index.php
@@ -8,7 +8,9 @@ require __DIR__ . '\partials\header.php';
 // On peut faire une requette SQL en PHP (la connexion a la db se fait via le heeader puis via la config)
 $query = db()->query('SELECT * FROM movie');
 
+// on execute la requete qui nous renvoie un tableau avec tous les eleemnts
 $movies = $query->fetchAll();
+
 var_dump($movies);
 ?>
 <?php
