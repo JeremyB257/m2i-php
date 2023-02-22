b0dce618218baf81ddb00780220cc1b3c50c3f82
Jerem <bertelootjerem@gmail.com>
2023-02-22 11:52:41 +0100

    db connection

--- 12-pdo-mysql/config/db.php
+++ 12-pdo-mysql/config/db.php
@@ -1,7 +1,9 @@
 <?php
+// definis les constantes de connexion a la DB
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'webflix');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
+// connexion a la db
 $db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
