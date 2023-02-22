29fc970a2820abb4fffc00777436b02543aaac32
Jerem <bertelootjerem@gmail.com>
2023-02-22 11:40:06 +0100

    init

--- 12-pdo-mysql/index.php
+++ 12-pdo-mysql/index.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr">
+<?php
+require __DIR__ . '\partials\header.php';
+?>
+<h1>Le titre</h1>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PDO Webflix</title>
-</head>
+<?php
+require __DIR__ . '\partials\footer.php';
 
-<body>
-
-</body>
-
-</html>
+?>
