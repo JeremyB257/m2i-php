e3f935a307f950549c94eafd96db3943e8bb75f8
Jerem <bertelootjerem@gmail.com>
2023-02-24 08:13:44 +0100

    exo

--- 12-pdo-mysql/contact.php
+++ 12-pdo-mysql/contact.php
@@ -13,7 +13,7 @@ if (!empty($_POST)) {
         $errors['name'] = 'Le nom est obligatoire.';
     }
     if (mb_strlen($message) < 15) {
-        $errors['name'] = 'Le message doit comporter 15 caracteres.';
+        $errors['message'] = 'Le message doit comporter 15 caracteres.';
     }
 
 
@@ -27,21 +27,44 @@ if (!empty($_POST)) {
     }
 }
 
+
+$messBdd = selectAll('SELECT * FROM contacts');
+
 ?>
 <div class="container-lg mt-3">
     <h1>Contactez nous</h1>
+    <?php
+    if (!empty($errors)) { ?>
+        <div class="alert alert-danger" role="alert">
+            <?php foreach ($errors as $error) { ?>
+                <p><?= $error ?></p>
+            <?php } ?>
+        </div>
+    <?php } else if (empty($errors) && !empty($_POST)) { ?>
+        <div class="alert alert-success" role="alert">
+            <p>Formulaire envoyer !</p>
+        </div>
+    <?php } ?>
     <form action="" method="post">
         <div class="mb-3">
             <label for="name" class="form-label">Nom</label>
-            <input type="text" name="name" class="form-control" id="name" aria-describedby="emailHelp">
+            <input type="text" name="name" class="form-control" id="name" value="<?= $name ?>">
         </div>
         <div class="mb-3">
             <label for="message" class="form-label">Message</label>
-            <input type="text" name="message" class="form-control" id="message">
+            <input type="text" name="message" class="form-control" id="message" value="<?= $message ?>">
         </div>
 
         <button type="submit" class="btn btn-primary">Envoyer</button>
     </form>
+    <?php foreach ($messBdd as $mess) { ?>
+        <div class="card m-2">
+            <div class="card-body">
+                <h5 class="card-title"><?= $mess['name'] ?></h5>
+                <p class="card-text"><?= $mess['message'] ?></p>
+            </div>
+        </div>
+    <?php } ?>
 </div>
 <?php
 require __DIR__ . '\partials\footer.php';
