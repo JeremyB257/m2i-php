a14cd572c363a1e0175d09538ec59a2f6201c99b
Jerem <bertelootjerem@gmail.com>
2023-02-24 10:01:48 +0100

    exo

--- 12-pdo-mysql/deleteContact.php
+++ 12-pdo-mysql/deleteContact.php
@@ -6,9 +6,9 @@ require __DIR__ . '\partials\header.php';
 
 $id = sanitize($_GET['id'] ?? null);
 
-if (is_int($id)) {
+if (!is_int($id)) {
     $query = db()->prepare('DELETE FROM contacts WHERE id = :id');
-    $query->execute(['id' => $id]);
+    $query->execute(['id' => intval($id)]);
 }
 
 header("Location: contact.php");
