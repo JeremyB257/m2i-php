4d3fe945d3aadb7b282a31a9ee62704ce7c7059d
Jerem <bertelootjerem@gmail.com>
2023-02-27 15:24:55 +0100

    14-fichiers

--- 14-fichiers/index.php
+++ 14-fichiers/index.php
@@ -9,7 +9,29 @@
 </head>
 
 <body>
+    <?php
+    // ouvrir un fichier en lecture + ecriture
+    $file = fopen('log.txt', 'a+');
+    //Ecrire dans un fichier
+    fwrite($file, "Salut\nCa va ?\n");
+    fclose($file);
 
+    //Lire le fichier en lecture seul
+    $file = fopen('log.txt', 'r');
+    $content = fread($file, filesize('log.txt'));
+    fclose($file);
+    // racourci
+    $content = file_get_contents('log.txt');
+    // recupere chaque ligne dans un tableau
+    $lines = array_filter(explode("\n", $content));
+
+    ?>
+    <h2>le ficghier à été modifié le <?= date('d/m/Y à H:i:s', filemtime('log.txt')) ?></h2>
+    <ul>
+        <?php foreach ($lines as $line) { ?>
+            <li><?= $line ?></li>
+        <?php } ?>
+    </ul>
 </body>
 
 </html>
