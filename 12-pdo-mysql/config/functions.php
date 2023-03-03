b629756d5e51201e7b0649bdf623beea94ff94c7
Jerem <bertelootjerem@gmail.com>
2023-03-03 14:02:14 +0100

    exo

--- 12-pdo-mysql/config/functions.php
+++ 12-pdo-mysql/config/functions.php
@@ -1,34 +1,40 @@
 <?php
 
-function truncate($text, $limit = 15) {
+function truncate($text, $limit = 15)
+{
     if (mb_strlen($text) > $limit) {
         return substr($text, 0, $limit) . '...';
     }
     return $text;
 }
 
-function format_date($date, $format = 'd/m/Y') {
+function format_date($date, $format = 'd/m/Y')
+{
     return date($format, strtotime($date));
 }
 
 
-function format_duration($duration) {
+function format_duration($duration)
+{
     return floor($duration / 60) . 'h' . (($duration % 60) < 10 ? ('0' . $duration % 60) : ($duration % 60));
 }
 
 
-function select($query, $exec = []) {
-    $quer = db()->prepare($query);
-    $quer->execute($exec);
-    return $quer->fetch();
+function select($sql, $exec = [])
+{
+    $query = db()->prepare($sql);
+    $query->execute($exec);
+    return $query->fetch();
 }
 
-function selectAll($sql, $exec = []) {
+function selectAll($sql, $exec = [])
+{
     $query = db()->prepare($sql);
     $query->execute($exec);
     return $query->fetchAll();
 }
 
-function sanitize($value) {
+function sanitize($value)
+{
     return trim(htmlspecialchars(($value ?? '')));
 }
