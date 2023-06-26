92dd447828b6931051d2df49f0c6602a777ce436
Jerem <bertelootjerem@gmail.com>
2023-06-26 10:12:54 +0200

    maj

--- 11-sql/base.sql
+++ 11-sql/base.sql
@@ -161,3 +161,31 @@ FROM `commande_ligne`
 GROUP BY nom 
 HAVING nbr_items > 1
 ORDER BY nbr_items DESC
+
+--Enregistrer le prix total à l’intérieur de chaque ligne des commandes, en fonction du prix unitaire et de la quantité
+
+--Obtenir le montant total pour chaque commande et y voir facilement la date associée à cette commande ainsi que le prénom et nom du client associé
+
+--(difficulté très haute) Enregistrer le montant total de chaque commande dans le champ intitulé “cache_prix_total”
+
+--Obtenir le montant global de toutes les commandes, pour chaque mois
+
+--Obtenir la liste des 10 clients qui ont effectué le plus grand montant de commandes, et obtenir ce montant total pour chaque client.
+
+--Obtenir le montant total des commandes pour chaque date
+
+--Ajouter une colonne intitulée “category” à la table contenant les commandes. Cette colonne contiendra une valeur numérique
+
+--Enregistrer la valeur de la catégorie, en suivant les règles suivantes :
+--“1” pour les commandes de moins de 200€
+--“2” pour les commandes entre 200€ et 500€
+--“3” pour les commandes entre 500€ et 1.000€
+--“4” pour les commandes supérieures à 1.000€
+
+--Créer une table intitulée “commande_category” qui contiendra le descriptif de ces catégories
+
+--Insérer les 4 descriptifs de chaque catégorie au sein de la table précédemment créée
+
+--Supprimer toutes les commandes (et les lignes des commandes) inférieur au 1er février 2019. Cela doit être effectué en 2 requêtes maximum
+
+
