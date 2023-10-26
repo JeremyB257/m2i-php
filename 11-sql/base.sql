be805f5d09f0b1c5901d50715947033462d5b89d
Jerem <bertelootjerem@gmail.com>
2023-10-26 12:48:12 +0200

    maj

--- 11-sql/base.sql
+++ 11-sql/base.sql
@@ -78,23 +78,30 @@ INNER JOIN movie AS m ON m.id_movie = mha.id_movie
 INNER JOIN category AS c ON c.id_category = m.id_category
 WHERE c.name = 'Action';
 
--- exo
+
+
+
+----------------------------------------------------------------------- exo Ville 1
+
+
+
+
 
 -- Obtenir la liste des 10 villes les plus peuplées en 2012
--- SELECT * FROM `villes_france_free` ORDER BY ville_population_2012 DESC LIMIT 10;
+SELECT * FROM `villes_france_free` ORDER BY ville_population_2012 DESC LIMIT 10;
 
 -- Obtenir la liste des 50 villes ayant la plus faible superficie
--- SELECT * FROM `villes_france_free` ORDER BY ville_surface ASC LIMIT 50;
+ SELECT * FROM `villes_france_free` ORDER BY ville_surface ASC LIMIT 50;
 
 -- Obtenir la liste des départements d’outres-mer, c’est-à-dire ceux dont le numéro de département commencent par “97”
--- SELECT * FROM `departement` WHERE departement_code LIKE '97%';
+ SELECT * FROM `departement` WHERE departement_code LIKE '97%';
 
 -- Obtenir le nom des 10 villes les plus peuplées en 2012, ainsi que le nom du département associé
---SELECT v.ville_nom, d.departement_nom 
---FROM `villes_france_free` AS v 
---INNER JOIN departement AS d ON v.ville_departement = d.departement_code
---ORDER BY v.ville_population_2012 DESC
---LIMIT 10;
+SELECT v.ville_nom, d.departement_nom 
+FROM `villes_france_free` AS v 
+INNER JOIN departement AS d ON v.ville_departement = d.departement_code
+ORDER BY v.ville_population_2012 DESC
+LIMIT 10;
 
 
 -- Obtenir la liste du nom de chaque département, associé à son code et du nombre de commune au sein de ces département, en triant afin d’obtenir en priorité les départements qui possèdent le plus de communes
@@ -144,6 +151,9 @@ SET ville_nom = REPLACE(ville_nom, '-', ' ')
 WHERE `ville_nom` LIKE 'SAINT-%'
 
 
+----------------------------------------------------------------------- exo Ville 2
+
+
 
 
 --Obtenir l’utilisateur ayant le prénom “Muriel” et le mot de passe “test11”, sachant que l’encodage du mot de passe est effectué avec l’algorithme Sha1.
@@ -166,33 +176,82 @@ HAVING nbr_items > 1
 ORDER BY nbr_items DESC
 
 --Enregistrer le prix total à l’intérieur de chaque ligne des commandes, en fonction du prix unitaire et de la quantité
+UPDATE `commande_ligne` 
+SET  `prix_total` = (`quantite` * `prix_unitaire`)
 
 --Obtenir le montant total pour chaque commande et y voir facilement la date associée à cette commande ainsi que le prénom et nom du client associé
+SELECT client.prenom, client.nom, commande.date_achat, commande_id, SUM(prix_total) AS prix_commande 
+FROM `commande_ligne` 
+LEFT JOIN commande ON commande.id = commande_ligne.commande_id
+LEFT JOIN client ON client.id = commande.client_id
+GROUP BY `commande_id`
 
 --(difficulté très haute) Enregistrer le montant total de chaque commande dans le champ intitulé “cache_prix_total”
+UPDATE commande AS t1 
+INNER JOIN 
+    ( SELECT commande_id, SUM(commande_ligne.prix_total) AS p_total 
+      FROM commande_ligne 
+      GROUP BY commande_id ) t2 
+          ON  t1.id = t2.commande_id 
+SET t1.cache_prix_total = t2.p_total
 
 --Obtenir le montant global de toutes les commandes, pour chaque mois
+SELECT YEAR(`date_achat`), MONTH(`date_achat`), SUM(`cache_prix_total`) 
+FROM `commande` 
+GROUP BY YEAR(`date_achat`), MONTH(`date_achat`)
+ORDER BY YEAR(`date_achat`), MONTH(`date_achat`)
 
 --Obtenir la liste des 10 clients qui ont effectué le plus grand montant de commandes, et obtenir ce montant total pour chaque client.
+SELECT client.nom, client.prenom, SUM(commande.cache_prix_total) AS client_montant
+FROM `commande` 
+LEFT JOIN client ON client.id = commande.client_id
+GROUP BY commande.client_id
+ORDER BY client_montant DESC
+LIMIT 10
 
 --Obtenir le montant total des commandes pour chaque date
+SELECT `date_achat`, SUM(`cache_prix_total`) 
+FROM `commande` 
+GROUP BY `date_achat`
 
 --Ajouter une colonne intitulée “category” à la table contenant les commandes. Cette colonne contiendra une valeur numérique
+ALTER TABLE `commande` ADD `category` TINYINT UNSIGNED NOT NULL AFTER `cache_prix_total`;
 
 --Enregistrer la valeur de la catégorie, en suivant les règles suivantes :
 --“1” pour les commandes de moins de 200€
 --“2” pour les commandes entre 200€ et 500€
 --“3” pour les commandes entre 500€ et 1.000€
 --“4” pour les commandes supérieures à 1.000€
+UPDATE `commande` 
+SET `category` = (
+  CASE 
+     WHEN cache_prix_total<200 THEN 1
+     WHEN cache_prix_total<500 THEN 2
+     WHEN cache_prix_total<1000 THEN 3
+     ELSE 4
+  END )
 
 --Créer une table intitulée “commande_category” qui contiendra le descriptif de ces catégories
+CREATE TABLE `commande_category` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --Insérer les 4 descriptifs de chaque catégorie au sein de la table précédemment créée
+INSERT INTO `commande_category` (`id`, `nom`) VALUES (1, 'commandes de moins de 200€');
+INSERT INTO `commande_category` (`id`, `nom`) VALUES (2, 'commandes entre 200€ et 500€');
+INSERT INTO `commande_category` (`id`, `nom`) VALUES (3, 'commandes entre 500€ et 1.000€');
+INSERT INTO `commande_category` (`id`, `nom`) VALUES (4, 'commandes supérieures à 1.000€');
 
 --Supprimer toutes les commandes (et les lignes des commandes) inférieur au 1er février 2019. Cela doit être effectué en 2 requêtes maximum
+DELETE FROM `commande_ligne` 
+WHERE `commande_id` IN ( SELECT id FROM commande WHERE date_achat < '2019-02-01' );
+DELETE FROM `commande` WHERE date_achat < '2019-02-01';
 
 
 
+----------------------------------------------------------------------- exo Actor
 
 
 -- From the following table, write a SQL query to find the actors who played a role in the movie 'Annie Hall'. Return all the fields of actor table.
@@ -417,7 +476,7 @@ WHERE mov_id IN (
         WHERE act_id IN (
             SELECT act_id
             FROM movie_cast GROUP BY act_id
-            HAVING COUNT(act_id)>1
+            HAVING COUNT(act_id) > 1
         )
     )
 )
